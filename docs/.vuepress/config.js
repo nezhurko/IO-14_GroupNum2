bb4eaa9fa06db0d3ad71dc75e89aece6f70c636b
nezhurko <timurmail27@gmail.com>
2023-05-14 00:05:52 +0300

    try to fix vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/IO-14_GroupNum2/',
+  base: '/edu_db_labs/',
   theme: 'cool',
   // dest: 'dist',
   head: [
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/nezhurko/IO-14_GroupNum2',
+    repo: 'https://github.com/edu-db/edu_db_labs',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
@@ -95,7 +95,7 @@ module.exports = {
     // editLinkText: 'Ви можете покращити цю сторінку'
 
   },
-  title: 'Система управління відкритими даними',
+  title: 'Назва проєкту',
   description: 'Лабораторні роботи з дисципліни "Організація баз даних"',
   configureWebpack: {
     resolve: {
